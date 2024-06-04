44d762ee083fc6842612683fa592b17f902ab512
Blum <roeschu.bluem@gmail.com>
2024-06-04 17:43:55 +0200

    Anpassung bash script

--- bash/setPublicIP.sh
+++ bash/setPublicIP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # setzen der public Ip4 für prometheus
 export EC2_IPV4_PUB=$(curl -s ifconfig.me)
-sudo sed -i "s/xxx.xxx.xxx.xxx/$EC2_IPV4_PUB/g" /home/ubuntu/prometheus/prometheus.yml
+sudo sed -i "s/xxx.xxx.xxx.xxx/$EC2_IPV4_PUB/g" /home/ubuntu/prometheus.yml
