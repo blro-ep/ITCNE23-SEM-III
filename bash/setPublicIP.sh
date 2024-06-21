60a6e09bdabde827d861ca5f737b897fdfff1532
Blum <roeschu.bluem@gmail.com>
2024-06-21 21:26:48 +0200

    update grafana

--- bash/setPublicIP.sh
+++ bash/setPublicIP.sh
@@ -3,3 +3,4 @@
 export EC2_IPV4_PUB=$(curl -s ifconfig.me)
 export EC2_IPV4_LOC=$(hostname -I | awk '{print $1}')
 sudo sed -i "s/xxx.xxx.xxx.xxx/$EC2_IPV4_LOC/g" /opt/ITCNE23-SEM-III/prometheus/prometheus.yml
+sudo sed -i "s/xxx.xxx.xxx.xxx/$EC2_IPV4_LOC/g" /opt/ITCNE23-SEM-III/grafana/data_source/prometheus.json
